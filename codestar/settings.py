c235d5d7ffe46e2ef3f9eab3d65d0df339e02c17
RockyPraxe <filipio.svanda@gmail.com>
2023-09-27 15:36:42 +0000

    Deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -29,9 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['8000-rockypraxe-ithinktheref-onrfrr8red7.ws-eu104.gitpod.io']
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+
 ALLOWED_HOSTS = ['blog-walktrough-e6f16f6a1892.herokuapp.com', 'localhost', '8000-rockypraxe-ithinktheref-onrfrr8red7.ws-eu104.gitpod.io',  '8000-rockypraxe-ithinktheref-onrfrr8red7.ws-eu105.gitpod.io']
 
 
@@ -59,6 +62,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger'
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
