3699ab74a14254efa243effc2cab5ca6b34b44d1
RockyPraxe <filipio.svanda@gmail.com>
2023-09-27 14:49:30 +0000

    Add crispy_forms

--- blog/admin.py
+++ blog/admin.py
@@ -3,14 +3,13 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 
@@ -23,4 +22,3 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
-
